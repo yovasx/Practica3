7a1c208e0b79fb450d49ce42da512f176a135fce
Marcosvc-hash <lpze.marcosjavier.vargas.ca@unifranz.edu.bo>
2026-09-07 21:05:33 -0400

    feat: eliminado logico de usuarios

--- src/main/java/com/unifranz/proyectointegrador/application/service/UsuarioService.java
+++ src/main/java/com/unifranz/proyectointegrador/application/service/UsuarioService.java
@@ -5,6 +5,7 @@ import com.unifranz.proyectointegrador.application.dto.UsuarioDto;
 import java.util.List;
 
 public interface UsuarioService {
-    UsuarioDto guardar (UsuarioDto usuarioDto);
+    UsuarioDto guardar(UsuarioDto usuarioDto);
     List<UsuarioDto> listar();
+    void eliminarLogico(Long id);
 }
